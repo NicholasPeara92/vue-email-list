f85e4b674c4426e5f8678220e2ea791d55731115
NicholasPeara92 <nicholas.peara@gmail.com>
2022-11-16 15:23:20 +0100

    Genera 10 email e li stampa all'interno della lista

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
 'use strict'
 
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
 const { createApp } = Vue
 
 createApp({
     data() {
         return {
-        message: 'Hello Vue!'
+        mails: []
+        }
+    },
+    methods: {
+
+    },
+    created() {
+        for(let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                this.mails.push(response.data.response);
+            });
+
         }
     }
 }).mount('#app')
